0a04048c4349909de58037992f982b654dfd676e
Johnny-Asmar <Johnny@beytek.co>
2023-11-15 20:11:24 +0200

    demo

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+def add(x, y):
 
-@app.route('/')
-def hello():
-    return 'Hello, Dockerized Flask App!'
+    return x + y
+ 
+def subtract(x, y):
+
+    return x - y
+
+@app.route('/addition')
+def addition():
+    x = int(request.args.get("x"))
+    y = int(request.args.get("y"))
+    return f"{add(x, y)}"
+
+@app.route('/substraction')
+def substraction():
+    x = int(request.args.get("x"))
+    y = int(request.args.get("y"))
+    return f"{subtract(x, y)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
